388e8df122f8ee5c0b734034eda3a8b0feba4161
FelipeCalle <felipe_calle_ricci96@hotmail.com>
2022-03-06 15:33:51 -0300

    pratica aula 2

--- ola.c
+++ ola.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 int main(int argc, char *argv[])
 {
-printf(“ola, %s”, argv[1]);
+char argumento[30];
+/* o valor padrão é Mundo */
+strcpy(argumento, argc > 2 ? argv[1]:"Mundo");
+printf("ola, %s !", argumento);
 return 0;
-}
+}   
+feito2
+feito3
+ramo 1
